a88c0f08c4d03f43c5eb195982deb09f4423a322
Satjot Sawhney <satjot@gmail.com>
2011-03-17 16:14:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmsScores::Application.config.secret_token = '8f5bf6ab3feff971af0e50697a43881bc9a160cb895f5f492fc8ef72eb753e4a06f81446b8c9a87d085a33856da859b84f9310b39ca1a5199c609539802908d9'
